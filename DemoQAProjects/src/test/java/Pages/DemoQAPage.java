323752c772e1635ff34f697adc7b219a19a02a85
Hussein Ahmed Tejan <tejanahmedhu@gmail.com>
2018-06-19 17:10:21 +0100

    added to Git

--- DemoQAProjects/src/test/java/Pages/DemoQAPage.java
+++ DemoQAProjects/src/test/java/Pages/DemoQAPage.java
@@ -1,5 +1,7 @@
 package Pages;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 public class DemoQAPage {
@@ -7,8 +9,17 @@ public class DemoQAPage {
 	@FindBy(xpath="//*[@id=\"menu-item-141\"]")
 	private WebElement droppableButton;
 	
+	@FindBy(xpath="//*[@id=\"draggableview\"]")
+	private WebElement droppableObj;
+	
 	public void click() {
 		droppableButton.click();
+		
+	}
+	
+	public void drop(WebDriver myWebdriver) {
+		Actions action = new Actions(myWebdriver);
+		action.moveToElement(droppableObj).clickAndHold().moveByOffset(200, 20).release().perform();;
 		
 	}
 }
--- DemoQAProjects/src/test/java/Tests/DroppableTest.java
+++ DemoQAProjects/src/test/java/Tests/DroppableTest.java
@@ -5,11 +5,13 @@ import static org.junit.Assert.assertEquals;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 
-//import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentReports;
 
 import org.junit.Test;
 
@@ -17,11 +19,13 @@ import Pages.*;
 public class DroppableTest {
 	
 	WebDriver myDriver;
+	static ExtentReports extent;
+	
 	
 	@BeforeClass
 	public static void init() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Desktop\\chromedriver.exe");
-//		extent= new ExtentReports("C:\\Users\\Admin\\Desktop\\extentDroppable.html", true);
+		extent= new ExtentReports("C:\\Users\\Admin\\Desktop\\extentDroppable.html", true);
 	}
 	
 	@Before
@@ -43,12 +47,22 @@ public class DroppableTest {
 		 DemoQAPage page = PageFactory.initElements(myDriver, DemoQAPage.class);     
 		 page.click();
 		 assertEquals("http://demoqa.com/droppable/",myDriver.getCurrentUrl());
+	
+		 page.drop(myDriver);
+		 try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		 assertEquals("Dropped!",myDriver.findElement(By.id("droppableview")).findElement(By.xpath("//*[@id=\"droppableview\"]/p")).getText());
+		 
 	}
 	
 	@After
 	public void tearDown() {
 		myDriver.close();
-//		extent.flush();
+		extent.flush();
 	}
 	
 }
